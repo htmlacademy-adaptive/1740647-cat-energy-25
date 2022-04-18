db9e20a41c137aaffd5021a1b4ce36e783a5a70a
Alexander <alex.kadoh@mail.ru>
2022-04-18 16:07:46 +0700

    refactor menu

--- source/scripts.js
+++ source/scripts.js
@@ -1,11 +1,9 @@
 const menuButton = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
+//Не понимаю для чего делать отдельный класс menu--nojs если можно программно сделать модификатор menu--closed. Который при загрузке JS будет вешаться.
+menu.classList.add('menu--closed');
 
 menuButton.addEventListener('click', () => {
   let menuIsClosed = menu.classList.contains('menu--closed');
-  if (menuIsClosed) {
-    menu.classList.remove('menu--closed');
-  } else {
-    menu.classList.add('menu--closed');
-  }
+  menuIsClosed? menu.classList.remove('menu--closed') : menu.classList.add('menu--closed');
 })
